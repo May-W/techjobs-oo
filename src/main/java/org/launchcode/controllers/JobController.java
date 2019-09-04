b381e0e1a8d00df93cc7404c1cb9d42422f6683c
May W <51608792+May-W@users.noreply.github.com>
2019-09-03 23:08:36 -0500

    Finished TODO 2-5, got add template displaying properly

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -41,6 +41,8 @@ public class JobController {
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
 
+        //asks us to use jobData.add(newJob)
+
         return "";
 
     }
